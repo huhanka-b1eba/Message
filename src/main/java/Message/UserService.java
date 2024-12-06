c7922baf9cb1a5ad2315fae5faa7857111f3a461
Aigiz Tulubaev <92015392+huhanka-b1eba@users.noreply.github.com>
2024-12-07 00:06:21 +0500

    Update UserService.java

--- src/main/java/Message/UserService.java
+++ src/main/java/Message/UserService.java
@@ -1,5 +1,12 @@
-package Message;
+import Message.User;
 
-public class User {
-
+public class UserService {
+    public static User[] create4Users(){
+        User[] arrayUser = {
+                new User("Cheater777", "Aleksander", "89234264177", "qwerty123"),
+                new User("aboba", "Aigiz", "89963359020", "Bashiriatop"),
+                new User("uecnheca", "Anastasia", "89993325676", "Nastya_2006"),
+                new User("kto ia?", "nikto", "00000000000", "Z1_Z2_Z3_Z4__ZZZZZZZZZZZ")};
+        return arrayUser;
+    }
 }
